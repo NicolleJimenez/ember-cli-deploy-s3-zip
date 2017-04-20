a7b1e36502b8ef1739621c1b4848b3ff8c143ed6
Nicolle Jimenez <nicolle.jimenez91@gmail.com>
2017-04-20 09:55:20 -0500

    [add] zip and upload ember dist directory to s3 as a ember deploy plugin

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 /* eslint-env node */
-'use strict';
+'use strict'
+const DeployPluginBase = require('ember-cli-deploy-plugin')
+const S3 = require('./lib/s3')
 
 module.exports = {
-  name: 'ember-cli-deploy-s3-zip'
-};
+  name: 'ember-cli-deploy-s3-zip',
+  createDeployPlugin: function (options) {
+    const DeployPlugin = DeployPluginBase.extend({
+      name: options.name,
+      defaultConfig: {
+        distDir: function (context) {
+          return context.distDir
+        }
+      },
+      requiredConfig: ['bucket', 'name', 'region'],
+      didDeploy: function (context) {
+        const bucket = this.readConfig('bucket')
+        const region = this.readConfig('region')
+        const accessKeyId = this.readConfig('accessKeyId')
+        const secretAccessKey = this.readConfig('secretAccessKey')
+        const zipPathS3 = this.readConfig('zipPathS3')
+        const name = this.readConfig('name')
+        const distDir = this.readConfig('distDir')
+        const s3 = new S3({ bucket, region, name, accessKeyId, secretAccessKey, plugin: this })
+
+        this.log(`creating zip from path=${distDir}`, { verbose: true })
+        return s3.createZip(distDir)
+          .then(() => s3.uploadZip(zipPathS3))
+          .then(() => {
+            this.log(`${name} already uploaded to s3 bucket=${bucket}`, { verbose: true })
+          })
+          .catch((error) => {
+            this.log(`Error: ${error}`, { color: 'red' })
+          })
+      }
+    })
+
+    return new DeployPlugin()
+  }
+}
